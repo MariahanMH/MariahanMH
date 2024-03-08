54d65e8533005a3a5e80f946a5c86ba136813f48
Mariahan <162399773+MariahanMH@users.noreply.github.com>
2024-03-07 19:53:16 -0700

    Add files via upload

---
+++ ejercicio-javascript/scripts/ejercicio.js
@@ -0,0 +1,11 @@
+function cambiarColor(){
+    variable_album = document.getElementById('album');/*selecciona el elemento album*/
+    variable_album.style.backgroundColor = "#30CBA4";
+
+}
+
+function cambiarTamano(){
+    variable_img = document.getElementsByClassName('imagen');
+    variable_img[0].style.height="150px";
+    variable_img[0].style.width="150px";
+}
